3707d9f7c1f925de35b55a72217dbac9365eb5c7
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-04-25 09:12:58 +0700

    Update bai6.component.ts

--- src/app/bai6/bai6.component.ts
+++ src/app/bai6/bai6.component.ts
@@ -9,20 +9,23 @@ import {FormsModule} from "@angular/forms";
   templateUrl: './bai6.component.html',
 })
 export class Bai6Component {
-  SoA: any;
-  SoB: any;
-  kq: any;
-  chon: any;
-
-  Tinh() {
-    if(isNaN(this.SoA) || isNaN(this.SoB)){
-      this.kq = "Vui lòng nhập số";
-      return;
+  randomArray: number[] = [];
+  ArrayNumeric: number[] = [];
+  N: any | 0;
+  generateRandomArray() {
+    const length = this.N;
+    let minValue = 1;
+    this.randomArray = [];
+    this.ArrayNumeric = [];
+    for (let i = 0; i < length; i++) {
+      const randomNumber = Math.floor(Math.random() * (100 - minValue + 1)) + minValue;
+      minValue = randomNumber + 1;
+      this.randomArray.push(randomNumber);
     }
-    if(Number(this.SoA)>Number(this.SoB)){
-      this.kq = this.SoA;
-    }else{
-      this.kq = this.SoB;
+    for(let i=0;i<this.randomArray.length;i++){
+      if(this.randomArray[i]%2!=0){
+        this.ArrayNumeric.push(this.randomArray[i]);
+      }
     }
   }
 }
