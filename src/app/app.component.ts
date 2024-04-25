a5a456fe8891c8423709bdbaab8402531495637c
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-04-25 09:12:20 +0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from "@angular/forms";
-import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+import {NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {NavComponent} from "./nav/nav.component";
 import {Bai3Component} from "./bai3/bai3.component";
 
@@ -12,12 +12,21 @@ import {Bai3Component} from "./bai3/bai3.component";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Lab2';
   namht = new Date().getFullYear();
   Array: any = []
+  isBai3Route: boolean = false;
   Add(item: string) {
     this.Array.push(item)
     console.log(this.Array)
+  }
+  constructor(private router: Router) {}
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isBai3Route = this.router.url.includes('Lab2_3.html');
+      }
+    });
   }
 }
