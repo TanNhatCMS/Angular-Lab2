b0016d80f9171009671f539fcc79a5f8f1daa38a
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-04-25 09:12:42 +0700

    Update bai4.component.ts

--- src/app/bai4/bai4.component.ts
+++ src/app/bai4/bai4.component.ts
@@ -1,23 +1,26 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-bai4',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './bai4.component.html'
 })
 export class Bai4Component {
-  isReadonly = true;
-
-  isDisabled = false;
-  toggleDisabled() {
-    this.isDisabled = !this.isDisabled;
-  }
-  toggleEnabled() {
-    this.isReadonly = !this.isReadonly;
-  }
-  myClick(){
-    alert("Hello");
+  randomArray: number[] = [];
+  N: any | 0;
+  kq: any;
+  generateRandomArray() {
+    const length = this.N;
+    const min = 1;
+    const max = 100;
+    this.randomArray = [];
+    for (let i = 0; i < length; i++) {
+      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      this.randomArray.push(randomNumber);
+    }
+    this.kq = this.randomArray.join(', ');
   }
 }
